bc419bfc5ddf6a6708390a0eb4e3bee5f53422df
Peter Larson <peter.e.larson@gmail.com>
2018-03-29 17:41:22 -0500

    Separate project into groups for MVC model. Create model for item.  Update checkbox to update from item not previous state. removed user defaults saving

--- Todoey/controller/TodoListViewController.swift
+++ Todoey/controller/TodoListViewController.swift
@@ -10,60 +10,80 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    var itemArray = ["Item 1", "Item 2", "Item 3"]
+    var itemArray = [Item]()
     
     //First way to persist data
-    let defaults = UserDefaults.standard
+    //let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //first method of persisting data
+        let newItem = Item(name: "Find Mike")
+        itemArray.append(newItem)
+        let newItem2 = Item(name: "Buy Eggs")
+        itemArray.append(newItem2)
+        let newItem3 = Item(name: "Take over the world")
+        itemArray.append(newItem3)
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
+        for num in 1...20 {
+            itemArray.append(Item(name: "Take over the world \(num)"))
         }
         
+        //first method of persisting data
+        //if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
+        //    itemArray = items
+        //}
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(itemArray[indexPath.row])
-        if(tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark){
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        
+        itemArray[indexPath.row].updateState()
+        
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    /**
+        returns number of rows in table
+    */
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //reusable cell seems to mess up check boxes
+        //we need to associate a property with the item, not the cell -> model
         let cell = tableView.dequeueReusableCell(withIdentifier: "todoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let curItem = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = curItem.itemName
+        
+        //using model to determine checkbox
+        cell.accessoryType = curItem.checked ? .checkmark : .none
+        
         return cell
     }
 
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        let alert = UIAlertController(title: "Add new Todoey Item", message: "", preferredStyle: .alert)
-        
+        //local variable to be used when updating list
         var textField = UITextField()
         
+        //alert that shows up when an item is added
+        let alert = UIAlertController(title: "Add new Todoey Item", message: "", preferredStyle: .alert)
+        
+        //makes alert have text field and saves entered value to textField variable
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
-            print(alertTextField.text)
             textField = alertTextField
-            
-            
         }
+        
+        //action that is called when ok is pressed
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once user hits add button on ui alert
-            self.itemArray.append(textField.text!)
+            self.itemArray.append(Item(name: textField.text!))
             
             //first way to persist data
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+           // self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
             
             self.tableView.reloadData()
---
+++ Todoey/model/Item.swift
@@ -0,0 +1,24 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Peter Larson on 3/29/18.
+//  Copyright © 2018 Peter Larson. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var itemName : String = ""
+    var checked : Bool = false
+    
+    init(name : String) {
+        itemName = name
+    }
+    
+    func updateState() -> Bool {
+        checked = !checked
+        print(itemName,"  ",checked)
+        return checked
+    }
+}
